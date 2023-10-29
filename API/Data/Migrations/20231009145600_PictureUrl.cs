70676398565d7bfd0873f8e59c08c06c22c98e12
tihac01 <you@example.com>
2023-10-29 20:56:44 +0100

    End of section 2

---
+++ API/Data/Migrations/20231009145600_PictureUrl.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PictureUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Products");
+        }
+    }
+}
