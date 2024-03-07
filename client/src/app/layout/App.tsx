5b438d3bb55d6421b27cb1295c0b82fe9f276c0c
tihac01 <you@example.com>
2024-03-07 21:09:40 +0100

    End of section 4

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaeaea' : '#121212'
+      }
+    }
+  })
+
+function handleThemeChange(){
+  setDarkMode(!darkMode);
+}
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Container>
+        <Outlet />
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,42 @@
+import { Avatar, CardMedia, Button, Card, CardActions, CardContent, Typography, CardHeader } from "@mui/material";
+import { Product } from "../../app/models/Product";
+import { Link } from "react-router-dom";
+
+interface Props {
+    product: Product
+}
+
+export default function ProductCard({ product }: Props) {
+    return (
+        <Card>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{bgcolor: 'secondary'}}>
+                        {product.name.charAt(0).toUpperCase()}
+                    </Avatar>
+                }
+                title= {product.name}
+                titleTypographyProps={{
+                    sx: {fontWeight: 'bold', color:'primary.main'}
+                }}
+            />
+            <CardMedia
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+                image= {product.pictureUrl}
+                title= {product.name}
+            />
+            <CardContent>
+                <Typography gutterBottom color='secondary' variant="h5">
+                    ${(product.price/100).toFixed(2)}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {product.brand} / {product.type}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Add to cart</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
+            </CardActions>
+        </Card>
+    )
+}
